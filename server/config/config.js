1ebaf9d7274b1f492ea07df4c3632a461dbece82
Gabriel Alejandro Martinez Labrador <34665792+gabomartinez21@users.noreply.github.com>
2019-04-20 23:17:42 -0500

    NPM start listo

--- server/config/config.js
+++ server/config/config.js
@@ -13,7 +13,7 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev'
 // ============================
 let urlDB;
 
-if(process.send.NODE_ENV === 'dev'){
+if(process.env.NODE_ENV === 'dev'){
      urlDB ="mongodb://localhost:27017/coffee";
 }else{
     urlDB ='mongodb+srv://pandatester:cpSCCykoOQgnz8C1@cluster0-bexgb.mongodb.net/coffe?retryWrites=true'
